30df972872b0266574f05c3653224c6cbfa8b319
smandapakaa <subrahmanyam.mbr@outlook.com>
2021-08-03 19:57:12 -0400

    Initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# node-mxz62g
+
+[Edit on StackBlitz ⚡️](https://stackblitz.com/edit/node-mxz62g)
---
+++ index.js
@@ -0,0 +1,10 @@
+const R = require('ramda');
+let input = [
+  { firstname: 'first1', lastname: 'last1', birthDate: 1 },
+  { firstname: 'first1', lastname: 'last1', birthDate: 1 },
+  { firstname: 'first2', lastname: 'last2', birthDate: 2 }
+];
+const strEq = R.eqProps(String);
+// console.log('output: ', R.uniqWith(R.eqProps, input));
+const data = Array.from(new Set(input.map(JSON.stringify))).map(JSON.parse);
+console.log(data);
